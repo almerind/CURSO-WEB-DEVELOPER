05ca1cde6a82335ddb114514d81223ed57aaf4c1
Almerindo Euclides Pacule <almerindoeuclidesp@gmail.com>
2023-09-27 17:37:17 +0200

    Fetch da Aula 4!!!

---
+++ JS/aula-03.js
@@ -0,0 +1,11 @@
+// Estruturas condicionais
+
+
+const idade = 20;
+const idade2 = '50';
+
+if(idade2 === 50){
+    console.log("Idade Menor 1")
+}else{
+    console.log("Idade Maior 2");
+}
---
+++ JS/aula-04.js
@@ -0,0 +1,11 @@
+const quantidade = 10;
+
+for(let i = 0; i < quantidade; i++)
+{
+    for(let k = 0; k < quantidade; k++)
+    {
+    
+        console.log(k)
+
+    }
+}
